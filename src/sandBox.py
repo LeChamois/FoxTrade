03f6a29244df749e37b60b40687c3eac05033879
0N17 <lino.portier@gmail.com>
2025-04-16 10:01:41 +0200

    Update sandBox.py

--- src/sandBox.py
+++ src/sandBox.py
@@ -2,3 +2,14 @@ from termcolor import cprint
 from mods.markets import get_historical_prices as getPrices
 
 print(*getPrices('BTCUSDT', '1s', 20))
+
+
+
+a = True
+b = False
+
+u = []
+v = []
+k = []
+
+result = [a if e in k else b for e in (v if v is not k else u)]
